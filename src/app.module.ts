8b44d55a9f62727d8ff005b19b2a5aaa375d0e8f
Christian Bachhelm <cbachhelm@tuta.io>
2023-07-22 18:25:15 +0200

    Add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { MongooseModule } from '@nestjs/mongoose'
+import { AuthModule } from './auth/auth.module'
 import { UsersModule } from './users/users.module'
 
 // MONOGDB_HOST should be equal to name of mongodb service defined in docker-compose
@@ -12,6 +13,7 @@ const { MONGODB_HOST, MONGODB_PORT, MONGODB_DATABASE } = process.env
     MongooseModule.forRoot(
       `mongodb://${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DATABASE}`,
     ),
+    AuthModule,
     UsersModule,
   ],
   controllers: [AppController],
