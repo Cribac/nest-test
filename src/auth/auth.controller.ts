12139a0ecaf5e010e87118285f94bd6ca6022778
Christian Bachhelm <cbachhelm@tuta.io>
2023-07-22 19:40:33 +0200

    Scaffold auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common'
+import { AuthService } from './auth.service'
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  // instead of using the Record<string, any> type,
+  // we should use a DTO class to define the shape of the request body. See the validation chapter for more information.
+  // @TODO: add DTO
+  // @TODO: add JWT Token
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async signIn(@Body() signInDto: Record<string, any>) {
+    return this.authService.validateUser(signInDto.email, signInDto.password)
+  }
+}
