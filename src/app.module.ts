1f6bb14cbde99d6034b1a31bfda2a5880e23803f
Christian Bachhelm <cbachhelm@tuta.io>
2023-07-22 14:20:01 +0200

    Connect to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,12 @@ import { AppService } from './app.service'
 import { MongooseModule } from '@nestjs/mongoose'
 import { UsersModule } from './users/users.module'
 
+// MONOGDB_HOST should be equal to name of mongodb service defined in docker-compose
 const { MONGODB_HOST, MONGODB_PORT, MONGODB_DATABASE } = process.env
 @Module({
   imports: [
     MongooseModule.forRoot(
-      `mongodb://${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DATABASE}`,
+      `mongodb://${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DATABASE}}`,
     ),
     UsersModule,
   ],
